47990cf34eb22119dbce4f1566397918cb3ae1b6
Alan Aliaga <alanaliagadev@gmail.com>
2018-09-21 12:51:09 -0400

    [Add] AvatarList - Avatar components

--- src/components/shared/AvatarList.js
+++ src/components/shared/AvatarList.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Avatar from './Avatar';
+
+const Wrapper = styled.section`
+  border: 4px solid #bada55;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+  grid-gap: 0.5rem;
+`;
 
 function renderAvatar(id) {
   return <Avatar key={id} id={id} />;
 }
 
 const AvatarList = ({ ids }) => {
-  return <div>{ids.map(renderAvatar)}</div>;
+  return <Wrapper>{ids.map(renderAvatar)}</Wrapper>;
 };
 
 export default AvatarList;
